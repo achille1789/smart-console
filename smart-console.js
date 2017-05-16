d3abe84a5209dfcc16c6d7fe7a1b60b422a0ad4f
achille1789 <vanni_k27@hotmail.com>
2017-05-16 10:33:36 +0100

    Added styles

--- smart-console.js
+++ smart-console.js
@@ -1,16 +1,16 @@
 var c = {
-    a: (...data) => console.assert(...data),
+    a: (text, ...extraParam) => console.assert(text, ...extraParam),
     c: () => console.clear(),
     cn: (data) => data ? console.count(data) : console.count(),
-    d: (...data) => console.debug(...data),
+    d: (text, ...extraParam) => console.debug(text, ...extraParam),
     dir: (data) => console.dir(data),    // non-standard
     dx: (data) => console.dirxml(data),   // non-standard
-    e: (...data) => console.error(...data),  //to test
+    e: (text, ...extraParam) => console.error(text, ...extraParam),  // tested
     g: () => console.group(),
     gc: () => console.groupCollapsed(),
     ge: () => console.groupEnd(),
-    i: (...data) => console.info(...data),
-    l: (...data) => console.log(...data),
+    i: (text, ...extraParam) => console.info(text, ...extraParam),
+    l: (text, ...extraParam) => console.log(text, ...extraParam),
     p: (data) => data ? console.profile(data) : console.profile(),
     pe: () => console.profileEnd(),
     tb: (data) => console.table(data),
@@ -18,5 +18,21 @@ var c = {
     te: (data) => data ? console.timeEnd(data) : console.timeEnd(),
     ts: (data) => data ? console.timeStamp(data) : console.timeStamp(),   // non-standard
     tr: () => console.trace(),   // to check
-    w: (...data) => console.warn(...data),
+    w: (text, ...extraParam) => console.warn(text, ...extraParam),
+
+    // Prefixed style log color
+    lS: (text, ...extraParam) => console.log("%c" + text, "color: #3c763d;", ...extraParam),
+    lI: (text, ...extraParam) => console.log("%c" + text, "color: #31708f;", ...extraParam),
+    lW: (text, ...extraParam) => console.log("%c" + text, "color: #8a6d3b;", ...extraParam),
+    lD: (text, ...extraParam) => console.log("%c" + text, "color: #a94442;", ...extraParam),
+    // Prefixed style log color + bold
+    lSB: (text, ...extraParam) => console.log("%c" + text, "color: #3c763d; font-weight: bold;", ...extraParam),
+    lIB: (text, ...extraParam) => console.log("%c" + text, "color: #31708f; font-weight: bold;", ...extraParam),
+    lWB: (text, ...extraParam) => console.log("%c" + text, "color: #8a6d3b; font-weight: bold;", ...extraParam),
+    lDB: (text, ...extraParam) => console.log("%c" + text, "color: #a94442; font-weight: bold;", ...extraParam),
+    // Prefixed style log background
+    lSBG: (text, ...extraParam) => console.log("%c" + text, "background-color: #dff0d8; color: #3c763d; font-weight: bold;", ...extraParam),
+    lIBG: (text, ...extraParam) => console.log("%c" + text, "background-color: #d9edf7; color: #31708f; font-weight: bold;", ...extraParam),
+    lWBG: (text, ...extraParam) => console.log("%c" + text, "background-color: #fcf8e3; color: #8a6d3b; font-weight: bold;", ...extraParam),
+    lDBG: (text, ...extraParam) => console.log("%c" + text, "background-color: #f2dede; color: #a94442; font-weight: bold;", ...extraParam),
 }
